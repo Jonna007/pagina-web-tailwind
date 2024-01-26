1f436f51579241838761155437c339b671af5386
Jonna007 <146044709+Jonna007@users.noreply.github.com>
2024-01-26 02:40:30 -0500

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderImgComponent } from './components/header-img/header-img.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderImgComponent, ProductsComponent, ButtonsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
